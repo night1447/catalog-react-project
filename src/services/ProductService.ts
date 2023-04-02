b47daf86a369dcfc66011fffb77eed481df48128
Dzmitry Solodov <solodow.mitya@yandex.by>
2023-04-02 17:50:58 +0300

    correct package and service

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -2,10 +2,11 @@ import {useHttp} from "../hooks/http.hooks";
 import {ICatalogProduct} from "../models/ICatalogProduct";
 import {useCallback} from "react";
 
+
 export const useProductService = () => {
     const {request, hasLoading, hasError, clearError} = useHttp();
     const getAllProducts = useCallback(async () => {
-        return await request('/data.json');
+		return await request(process.env.PUBLIC_URL+ '/data.json');
     }, [request])
     const transformProduct = (product: ICatalogProduct) => {
         return {
@@ -13,7 +14,7 @@ export const useProductService = () => {
         }
     }
     const getProductById = useCallback(async (id: number) => {
-        const result = await request('/data.json');
+        const result = await request(process.env.PUBLIC_URL + '/data.json');
         return transformProduct(result[result.findIndex((item: ICatalogProduct) => item.id === id)]);
     }, [request])
     return {getAllProducts, getProductById, hasError, hasLoading, clearError}
